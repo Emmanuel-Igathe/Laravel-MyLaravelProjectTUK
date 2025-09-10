b3f0409ce549cb1b6c3c059e4697806954f10e42
Emmanuel Igathe <emmanueligathe4@gmail.com>
2025-09-10 21:05:34 +0300

    MenController.php

--- MenController.php
+++ MenController.php
@@ -1,15 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-use App\Models\Men;
+use App\Models\Product; // Ensure you import the Product model
+
+use App\Models\men;
 
 class MenController extends Controller
 {
     public function index()
     {
-        $products = Men::where('category' , 'men')->get();
+        $products = Men::all(); // Use the Men model directly//Fetch men products from database
         return view('pages.men', compact('products'));
     }
 }
