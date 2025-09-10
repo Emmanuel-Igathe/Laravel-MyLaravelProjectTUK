55fd01d6ea08012b7b4cf85cb8f826b462d76417
Emmanuel Igathe <emmanueligathe4@gmail.com>
2025-09-10 21:11:41 +0300

    Men.php

--- Men.php
+++ Men.php
@@ -4,38 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Men extends Model
+class men extends Model
 {
-    /**
-     * The table associated with the model.
-     *
-     * @var string
-     */
     protected $table = 'men';
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
+     protected $fillable = [
         'name',
-        'image_url',
+        'image_url', 
         'reviews',
         'old_price',
         'new_price',
         'discount',
         'category'
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'old_price' => 'decimal:2',
-        'new_price' => 'decimal:2',
-        'reviews' => 'integer'
     ];
 }
