dfd946e2b0e10762d6119f653beb06428eb382b7
Emmanuel Igathe <emmanueligathe4@gmail.com>
2025-09-10 21:16:26 +0300

    2025_08_17_094507_create_men_table.php

--- 2025_08_17_094507_create_men_table.php
+++ 2025_08_17_094507_create_men_table.php
@@ -12,7 +12,6 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('men', function (Blueprint $table) {
-            $table->id();
             $table->string('name');
             $table->string('image_url');
             $table->integer('reviews')->default(0);
